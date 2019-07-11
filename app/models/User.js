d4b6ae8001954132084c78132213f20757d905de
julio Denis <julio_denis@live.com>
2019-07-11 09:03:19 -0500

    resolvers editado

--- app/models/User.js
+++ app/models/User.js
@@ -6,7 +6,6 @@ const UserSchema = new Schema({
 
 
 
-
 });
 
 module.exports = mongoose.model(UserSchema);
--- app/resolvers/Muatation.js
+++ app/resolvers/Muatation.js
@@ -5,8 +5,10 @@ const UserModel = require('../models/User');
 const createHouse = async (root, params, context, info) => {
 
     const newHouse = await HouseModel(params, data)
+        .catch(e => { throw new Error('Ocurrió un problema') })
+    if (!newHouse) throw new Error('No se pudo crear');
 
-    return newHouse
+    return newHouse.toObject();
 
 },
 
@@ -15,7 +17,6 @@ const updateInfo = async (root, params, context, info) => {
 
 
 },
-
 
 
 
